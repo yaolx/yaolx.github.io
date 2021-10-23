d459f3e77e6a90df9785329195bf4996fa8b4c52
yaolx <644894056@qq.com>
2021-10-23 18:25:14 +0800

    feature: 1.增加环境配置，放在config目录中 2.antd样式全量加载改成按组件按需加载 3.vite插件封装 4.服务配置

---
+++ config/constant.ts
@@ -0,0 +1,16 @@
+// 开发端口
+export const PORT = 1314
+// 请求接口地址
+export const VITE_PROXY_HTTP = 'http://localhost:1314'
+// 开启 mock
+export const VITE_APP_MOCK = true
+// 开启包依赖分析 可视化
+export const VITE_APP_ANALYZE = false
+// 开启Gzip压缩
+export const VITE_APP_COMPRESS_GZIP = false
+// 开启Gzip压缩，删除原文件
+export const VITE_APP_COMPRESS_GZIP_DELETE_FILE = false
+// 去除 console
+export const VITE_DROP_CONSOLE = true
+// 开启兼容
+export const VITE_APP_LEGACY = true
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,61 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import eslint from '@rollup/plugin-eslint'
-import typescript from '@rollup/plugin-typescript'
-import path from 'path'
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    {
-      ...eslint({
-        include: 'src/**/*.+(js|jsx|ts|tsx)'
-      }),
-      enforce: 'pre'
+import { ConfigEnv } from 'vite'
+import fs from 'fs'
+import dotenv from 'dotenv'
+import path from 'path'
+import { createVitePlugins } from './config/vite/plugins'
+import { createProxy } from './config/vite/proxy'
+import { PORT } from './config/constant'
+/**
+ * 环境初始化
+ * @param mode
+ */
+function envInit(mode) {
+  const envFile = `./config/env/.env.${mode}`
+  try {
+    fs.accessSync(envFile, fs.constants.F_OK)
+    const envConfig = dotenv.parse(fs.readFileSync(envFile))
+    for (const k in envConfig) {
+      if (Object.prototype.hasOwnProperty.call(envConfig, k)) {
+        process.env[k] = envConfig[k]
+      }
+    }
+  } catch (error) {
+    console.log('配置文件不存在，忽略')
+  }
+}
+export default ({ command, mode }: ConfigEnv) => {
+  const isBuild = command === 'build'
+  envInit(mode)
+  return {
+    plugins: createVitePlugins(mode, isBuild),
+    resolve: {
+      alias: {
+        '@/': path.resolve(__dirname, './src')
+      }
     },
-    reactRefresh(),
-    typescript()
-  ],
-  resolve: {
-    alias: {
-      '@/': path.resolve(__dirname, '["error", 2]')
+    // css 配置
+    css: {
+      preprocessorOptions: {
+        less: {
+          // 支持内联 JavaScript
+          javascriptEnabled: true,
+          // antd 定制主题样式
+          modifyVars: {
+            '@fill-body': '#fff'
+          }
+        }
+      },
+      modules: {
+        localsConvention: 'camelCase'
+      }
+    },
+    // 开发环境开启代理
+    server: {
+      host: true,
+      port: PORT,
+      open: true,
+      proxy: createProxy(),
     }
   }
-})
+}
