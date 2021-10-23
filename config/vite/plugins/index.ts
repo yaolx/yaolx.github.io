294a6c48ddaf4879f18ff71e99658f8a10d9f5ae
yaolx <644894056@qq.com>
2021-10-23 18:26:08 +0800

    feature: vite插件加载

---
+++ config/vite/plugins/index.ts
@@ -0,0 +1,48 @@
+/**
+ * vite plugin
+ */
+
+import { Plugin } from 'vite'
+import viteCompression from 'vite-plugin-compression'
+import legacy from '@vitejs/plugin-legacy'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import eslint from '@rollup/plugin-eslint'
+import typescript from '@rollup/plugin-typescript'
+
+import {
+  VITE_APP_ANALYZE,
+  VITE_APP_COMPRESS_GZIP
+} from '../../constant'
+import configMockPlugin from './mock'
+import configVisualizerPlugin from './visualizer'
+import configStyleImportPlugin from './styleImport'
+
+export function createVitePlugins(viteEnv: string, isBuild: boolean) {
+  const vitePlugins: (Plugin | Plugin[])[] = [
+    {
+      ...eslint({
+        include: 'src/**/*.+(js|jsx|ts|tsx)'
+      }),
+      enforce: 'pre'
+    },
+    reactRefresh(),
+    typescript(),
+    legacy(),
+    configStyleImportPlugin()
+  ]
+
+  // mock下开启
+  viteEnv === 'mock' && vitePlugins.push(configMockPlugin(isBuild))
+
+  // 包分析
+  VITE_APP_ANALYZE && vitePlugins.push(configVisualizerPlugin())
+
+  // 发布，打包
+  if (VITE_APP_COMPRESS_GZIP && isBuild) {
+    vitePlugins.push(
+      viteCompression({ deleteOriginFile: true })
+    )
+  }
+
+  return vitePlugins
+}
