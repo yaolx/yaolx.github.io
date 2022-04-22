a4aff8c62616dcdd36c85a7eb28ee44a38cf8f8a
yaolx <644894056@qq.com>
2022-04-23 07:50:52 +0800

    feat: 菜单组件优化

--- src/component/layout/header.tsx
+++ src/component/layout/header.tsx
@@ -2,24 +2,44 @@ import React from 'react'
 import { Layout, Menu } from 'antd'
 import { useNavigate } from 'react-router-dom'
 import styles from './styles/index.module.less'
+import { map } from 'lodash'
 const { Header } = Layout
-
+const menus = [
+  {
+    key: '',
+    title: '首页'
+  },
+  {
+    key: 'front',
+    title: '前端'
+  },
+  {
+    key: 'end',
+    title: '后端'
+  },
+  {
+    key: 'about',
+    title: '关于'
+  },
+  {
+    key: 'log',
+    title: '更新日志'
+  }
+]
 export default function HeaderLayout() {
   const navigate = useNavigate()
   const onSelectMenu = ({ key }) => {
-    navigate(key)
+    navigate(`/${key}`)
   }
   return (
-    <Layout>
-      <Header className={styles.header}>
-        <div className="logo" />
-        <Menu mode="horizontal" onClick={onSelectMenu}>
-          <Menu.Item key="/md">前端开发</Menu.Item>
-          <Menu.Item key="/">后端开发</Menu.Item>
-          <Menu.Item key="/md1">工具</Menu.Item>
-          <Menu.Item key="/md2">关于我</Menu.Item>
-        </Menu>
-      </Header>
-    </Layout>
+    <Header className={styles.header}>
+      <div className={styles.logo} />
+      <div className={styles.name}>千禮之行</div>
+      <Menu className={styles.menu} mode="horizontal" onClick={onSelectMenu}>
+        {map(menus, (menu) => {
+          return <Menu.Item key={menu.key}>{menu.title}</Menu.Item>
+        })}
+      </Menu>
+    </Header>
   )
 }
