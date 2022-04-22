447914da4d24bd7b0993dd570857b6d2086e5c72
yaolx <644894056@qq.com>
2022-04-22 22:55:07 +0800

    fix: 调整路由path的规则

--- src/component/mdx-menus/index.tsx
+++ src/component/mdx-menus/index.tsx
@@ -12,15 +12,15 @@ const mdxs = import.meta.globEager('../../page/md/*.mdx')
 // 解析md文件夹下的markdown文件，生成路由
 const MdxsRouter: mdxRouter[] = map(mdxs, (mdx, key) => {
   // 根据匹配路径获取文件名，用来当path
+  console.log('###', mdxs)
   const parttern: string = key.replace(/..\/..\/page\/md\/(.*).mdx/, function (regexp, replacement) {
     return replacement
   })
   const name = parttern.split('-')[0] || ''
   const date = parttern.split('-')[1] || ''
-  const path = date + parseInt(Math.random() * 10000 + '1')
   const MdxComponent = mdx.default
   return {
-    path: `/md/${path}`,
+    path: `/md/${date}`,
     name,
     date,
     element: (
