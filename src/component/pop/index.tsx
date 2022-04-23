a6799b0ed2daa7363845ce94d1e27a76168a82b0
yaolx <644894056@qq.com>
2022-04-23 16:51:53 +0800

    feat: 增加侧边弹出框组件

---
+++ src/component/pop/index.tsx
@@ -0,0 +1,43 @@
+import React, { useState, forwardRef, useRef, useImperativeHandle } from 'react'
+import { createPortal } from 'react-dom'
+import { isMobile } from '@/utils/browse'
+
+interface PopProps {
+  showPopMenu: () => void
+  children: JSX.Element
+}
+// 移动端菜单弹出框
+function PopMenu(props: PopProps) {
+  const { showPopMenu, children } = props
+  return (
+    <div className={`popup ${isMobile() ? 'h5' : 'pc'}`}>
+      <div className="popup-mask" onClick={showPopMenu} />
+      <div className="popup-body">{children}</div>
+    </div>
+  )
+}
+interface PopRef {
+  children: JSX.Element
+}
+const Pop = forwardRef((props: PopRef, ref) => {
+  const popRef = useRef<any>()
+  const [visible, setVisible] = useState(false)
+  function showPopMenu() {
+    setVisible(!visible)
+  }
+  useImperativeHandle(
+    ref,
+    () => ({
+      /* ref 打开 */
+      show() {
+        setVisible(true)
+      },
+      hide() {
+        setVisible(false)
+      }
+    }),
+    [popRef]
+  )
+  return visible ? createPortal(<PopMenu showPopMenu={showPopMenu}>{props.children}</PopMenu>, document.querySelector('body') as HTMLBodyElement) : null
+})
+export default Pop
