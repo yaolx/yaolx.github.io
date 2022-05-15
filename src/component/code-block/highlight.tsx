9a13da2f41a9508bcbbec0a78fa038a7a98fc408
yaolx <64489456@qq.com>
2022-05-15 18:29:20 +0800

    feat：引入代码片段高亮组件

---
+++ src/component/code-block/highlight.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import Highlight, { defaultProps } from 'prism-react-renderer'
+import type { Language } from 'prism-react-renderer'
+import theme from 'prism-react-renderer/themes/github'
+
+// copied from https://mdxjs.com/guides/syntax-highlighting
+
+interface Props {
+  readonly className?: string
+  readonly children?: string
+  readonly style?: React.CSSProperties
+}
+
+const HighLightCode = ({ children = '', className = '', style: propStyle }: Props) => {
+  const language = className?.replace(/language-/, '') as Language
+  return (
+    <Highlight {...defaultProps} theme={theme} code={children.trim()} language={language}>
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={propStyle ? { ...style, ...propStyle } : style}>
+          {tokens.map((line, i) => (
+            <div key={i} {...getLineProps({ line, key: i })}>
+              {line.map((token, key) => (
+                <span key={key} {...getTokenProps({ token, key })} />
+              ))}
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  )
+}
+
+export default HighLightCode
