c6cdb09c015db3a2cc79f6d904467b51048419d3
yaolx <644894056@qq.com>
2022-09-25 13:09:29 +0000

    Deploying to gh-pages from @ yaolx/yaolx.github.io@beaa3ec2a3711b0ff0bcb643001bb6f64f84cade 🚀

---
+++ assets/20220805@移动端自适应方案.2c0d582e.js
@@ -0,0 +1,12 @@
+import{v as u}from"./vendor.e6348de1.js";const F={},l="wrapper";function n({components:e,...E}){return u(l,{...F,...E,components:e,mdxType:"MDXLayout"},u("h1",null,u("center",null,"\u79FB\u52A8\u7AEF\u81EA\u9002\u5E94\u65B9\u6848")),u("h2",null,"\u79FB\u52A8\u7AEF\u81EA\u9002\u5E94"),u("p",null,"\u4E0B\u9762\u4EE5 iphone se \u4E3A\u4F8B\uFF0C\u5B83\u7684\u5206\u8FA8\u7387\u662F 375 ","*",` 667\uFF0Cdpr = 2
+\u8BBE\u8BA1\u7A3F\u7684\u5C3A\u5BF8\uFF1A750px`),u("h3",null,"\u65B9\u6848\u4E00\uFF1Aflexible+rem \u5B9E\u73B0"),u("p",null,"\u5728\u9875\u9762\u4E2D\u5F15\u5165",u("inlineCode",{parentName:"p"},'<script src="http://g.tbcdn.cn/mtb/lib-flexible/0.3.4/??flexible_css.js,flexible.js"><\/script>')),u("p",null,"\u4F1A\u5728",u("inlineCode",{parentName:"p"},'<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'),`\u4E2D
+\u5C06`,u("inlineCode",{parentName:"p"},"initail-scale"),`\u8BBE\u4E3A 0.5
+\u5C06 html \u7684 font-size \u8BBE\u4E3A 75px
+\u6211\u4EEC\u5728\u6837\u5F0F\u4E2D\u9700\u8981\u6839\u636E\u8BBE\u8BA1\u7A3F\u6765\u8BA1\u7B97\u5B9E\u9645\u7684 rem
+\u6BD4\u5982\u8BBE\u8BA1\u7A3F\u4E2D\u7684\u56FE\u7247\u662F 180 `,"*"," 180\uFF0C\u90A3\u4E48\u5BF9\u5E94\u5B83\u7684 rem \u662F 180 / 75 = 2.4rem"),u("h3",null,"\u65B9\u6848\u4E8C\uFF1Avm+rem \u5B9E\u73B0"),u("p",null,"\u65E0\u9700\u5F15\u5165\u4EFB\u4F55\u7684 js \u5E93"),u("p",null,`html \u7684 style \u5C5E\u6027\u7684 font-size \u8BBE\u7F6E\u4E3A 13.333333vw
+css \u4E2D 20px \u6539\u5199\u4E3A 0.2rem \u5373\u53EF
+\u8FD9\u4E48\u8BA1\u7B97\u7684\u5462\uFF1F
+750px = 100vw
+1px = 0.13333333vw
+100px = 13.333333vw`),u("p",null,`\u53C2\u8003
+`,u("a",{parentName:"p",href:"https://juejin.cn/post/6844904029898670088"},"\u90FD 2020 \u79FB\u52A8\u7AEF\u9002\u914D\u4F60\u8FD8\u7528 flexible.js \u5417\uFF1Fvw+rem \u4E00\u884C\u4EE3\u7801\u641E\u5B9A")))}n.isMDXComponent=!0;export{n as default};
