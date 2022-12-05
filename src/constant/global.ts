418a117ee6d42d34f4e40b23b6f6ae3899c2fe5f
yaolx <64489456@qq.com>
2022-12-05 18:08:09 +0800

    fix:：调整地址

--- src/constant/global.ts
+++ src/constant/global.ts
@@ -123,7 +123,7 @@ export const SITE_CATEGORY: Site[] = [
       },
       {
         id: 'electron',
-        href: 'https://www.electronjs.org/zh/',
+        href: 'https://www.electronjs.org/',
         icon: 'https://www.electronjs.org/zh/assets/img/logo.svg',
         title: 'electron'
       },
