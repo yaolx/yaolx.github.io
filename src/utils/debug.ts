1e72eeeb390386f2d9cc3a4ce3bec46b4ef6c667
yaolx <64489456@qq.com>
2023-06-06 15:32:59 +0800

    fix：线上调试优化

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,16 +1,25 @@
-import { URLQueryParserUtils } from '@yaolx/utils'
-
 export function startDebug() {
   // 获取search中的参数
-  const { debug } = URLQueryParserUtils.resolveQuery()
+  const params = new URLSearchParams(window.location.search)
+  const debug = params.get('debug')
   if (debug === '1') {
     window.localStorage.setItem('debug', '1')
   }
-  // 本地开发或者url中有带debug=1，则开启vconsole调试
+  // 本地开发或者url中有带debug=1，则开启eruda调试
   if (window.localStorage.getItem('debug') === '1') {
+    const debugTool = params.get('debug_tool') || 'eruda'
     // 动态引入，避免被打包到vendor中
-    import('vconsole').then((VConsole) => {
-      new VConsole.default()
-    })
+    // === eruda实现
+    if (debugTool === 'eruda') {
+      import('eruda').then((eruda) => {
+        eruda.default.init()
+      })
+    }
+    // === vconsole实现
+    if (debugTool === 'vconsole') {
+      import('vconsole').then((VConsole) => {
+        new VConsole.default()
+      })
+    }
   }
 }
